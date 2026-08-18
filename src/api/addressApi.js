de707ca6c96318a3147459e790096c3ecb0c4757
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-18 14:14:22 +0530

    Update addressApi.js

--- src/api/addressApi.js
+++ src/api/addressApi.js
@@ -5,11 +5,11 @@ export const getAddresses = async () => {
 };
 export const createAddress = async (addressData) => {
   const response = await api.post("/addresses", addressData);
-  return response.data.data;
+  return response.data;
 };
 export const updateAddress = async (id, addressData) => {
   const response = await api.put(`/addresses/${id}`, addressData);
-  return response.data.data;
+  return response.data;
 };
 export const deleteAddress = async (id) => {
   const response = await api.delete(`/addresses/${id}`);
