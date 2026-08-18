aaf78a467ee13be06882f61bb4c956b2ef468189
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-18 13:16:30 +0530

    Update addressApi.js

--- src/api/addressApi.js
+++ src/api/addressApi.js
@@ -1,20 +1,16 @@
 import api from "./axios";
-
 export const getAddresses = async () => {
   const response = await api.get("/addresses");
-  return response.data;
+  return response.data.data;
 };
-
 export const createAddress = async (addressData) => {
   const response = await api.post("/addresses", addressData);
-  return response.data;
+  return response.data.data;
 };
-
 export const updateAddress = async (id, addressData) => {
   const response = await api.put(`/addresses/${id}`, addressData);
-  return response.data;
+  return response.data.data;
 };
-
 export const deleteAddress = async (id) => {
   const response = await api.delete(`/addresses/${id}`);
   return response.data;
