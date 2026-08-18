31d7dc8afd232e86180ec87cacf764b061b3429d
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-18 12:33:17 +0530

    Update cartApi.js

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -2,24 +2,22 @@ import api from "./axios";
 
 export const getCart = async () => {
   const response = await api.get("/cart");
-  response.data.data || response.data
+  return response.data.data;
 };
 export const addCartItem = async (cartItemData) => {
   const response = await api.post("/cart/items", cartItemData);
-  return response.data;
+  return response.data.data;
 };
-
 export const updateCartItem = async (cartItemId, data) => {
   const response = await api.patch(`/cart/items/${cartItemId}`, data);
-  return response.data;
+  return response.data.data;
 };
-
 export const removeCartItem = async (cartItemId) => {
   const response = await api.delete(`/cart/items/${cartItemId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const clearCart = async () => {
   const response = await api.delete("/cart");
-  return response.data;
+  return response.data.data;
 };
