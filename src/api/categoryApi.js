dad2c899707bcd12bbdeee26065548337838f2ed
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-18 11:00:06 +0530

    Update categoryApi.js

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -1,16 +1,13 @@
 import api from "./axios";
-
 export const getCategories = async (topLevelOnly = false) => {
   const response = await api.get("/categories", {
     params: {
       topLevelOnly,
     },
   });
-console.log("Categories:", response);
-  return response.data;
+  return response.data.data || [];
 };
-
 export const getCategoryById = async (id) => {
   const response = await api.get(`/categories/${id}`);
-  return response.data;
+  return response.data.data || response.data;
 };
