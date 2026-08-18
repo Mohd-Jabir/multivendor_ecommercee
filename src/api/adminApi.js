c35df097c67608cd7203f2879d8ee76befbcff44
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-18 10:46:00 +0530

    Update adminApi.js

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,17 +1,19 @@
 import api from "./axios";
-
 export const getPendingVendors = async () => {
   const response = await api.get("/admin/vendors/pending");
-  return response.data;
+
+  return response.data.data || [];
 };
 
 export const getApprovedVendors = async () => {
   const response = await api.get("/admin/vendors/approved");
-  return response.data;
+
+  return response.data.data || [];
 };
 
 export const approveVendor = async (vendorId) => {
   const response = await api.post(`/admin/vendors/${vendorId}/approve`);
+
   return response.data;
 };
 
@@ -20,17 +22,22 @@ export const rejectVendor = async (vendorId) => {
 
   return response.data;
 };
+
 export const createAdminCategory = async (categoryData) => {
   const response = await api.post("/admin/categories", categoryData);
+
   return response.data;
 };
+
 export const updateAdminCategory = async (id, categoryData) => {
   const response = await api.put(`/admin/categories/${id}`, categoryData);
+
   return response.data;
 };
 
 export const deleteAdminCategory = async (id) => {
   const response = await api.delete(`/admin/categories/${id}`);
+
   return response.data;
 };
 
